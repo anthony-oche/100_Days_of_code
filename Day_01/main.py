f07cab95510679fe7611d4d19655a807c14f1ded
Anthony <ocheanthonyowoicho@gmail.com>
2025-06-25 19:26:06 +0100

    BAND NAME GENETATOR

---
+++ Day_01/main.py
@@ -0,0 +1,9 @@
+#welcome the user
+print("Welcome to the Band Name Generator")
+
+#prompt the user
+user_city = input("What's the name of the city you grew up in?\n")
+user_pet_name = input("What's your pet's name?\n")
+
+#Generate the band name
+print(f"Your band name could be {user_city} {user_pet_name}")
